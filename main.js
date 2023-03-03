6b7d169f2193e447664a57524d64945758371623
Vaishnavi More <93897423+Vaishnavi-More@users.noreply.github.com>
2023-03-03 14:17:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', { 
+    loop:true, 
+ 
+     // If we need pagination 
+  pagination: { 
+    el: '.swiper-pagination', 
+  }, 
+ 
+    // Navigation arrows 
+    navigation: { 
+        nextEl: '.swiper-button-next', 
+        prevEl: '.swiper-button-prev', 
+    }, 
+ 
+ 
+})
+
+const swiper = new Swiper('.swiper1', { 
+  loop:true, 
+
+   // If we need pagination 
+pagination: { 
+  el: '.swiper-pagination1', 
+}, 
+
+  // Navigation arrows 
+  navigation: { 
+      nextEl: '.swiper-button-next1', 
+      prevEl: '.swiper-button-prev1', 
+  }, 
+
+
+})
---
+++ out.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper1', { 
+  loop:true, 
+
+   // If we need pagination 
+pagination: { 
+  el: '.swiper-pagination1', 
+}, 
+
+  // Navigation arrows 
+  navigation: { 
+      nextEl: '.swiper-button-next1', 
+      prevEl: '.swiper-button-prev1', 
+  }, 
+
+
+})
